860250541c5280acfbd65aae0cc13aa5a6cefec3
池田和紀 <kazuki0801kyh@akane.waseda.jp>
2026-01-31 16:43:21 +0900

    Update Vite base to /volley-pwa/

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.v3jkpmttjto"
+    "revision": "0.fondsvm1tuo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
